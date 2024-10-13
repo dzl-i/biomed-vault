220fd90b7070c65c7fa89bac2c301dce89e551d7
dzl-i <denzel.iskandar@gmail.com>
2024-10-14 04:17:02 +1100

    feat: added error checking before updating researcher data

--- server/src/update/researcher.ts
+++ server/src/update/researcher.ts
@@ -1,9 +1,11 @@
 import { updateResearcherData } from "../helper/updateHelper";
 
 export async function updateResearcher(researcherId: string, id: string, researcherData: any) {
+  // Error checking
+  if (researcherId !== id) throw { status: 401, message: "You cannot edit other researcher's data." };
+
   const researcher = await updateResearcherData(id, researcherData);
   if (researcher === null) throw { status: 400, message: "Invalid researcher." };
-  if (researcher.id !== researcherId) throw { status: 401, message: "You cannot edit other researcher's data." };
 
   return {
     name: researcher.name,
