e86adff0aa0863804ff133f7d446df980f158605
dzl-i <denzel.iskandar@gmail.com>
2024-10-14 04:17:27 +1100

    feat: added error checking before updating patient data

--- server/src/update/patient.ts
+++ server/src/update/patient.ts
@@ -1,9 +1,14 @@
+import { getPatientDetails } from "../helper/patientHelper";
 import { updatePatientData } from "../helper/updateHelper";
 
 export async function updatePatient(researcherId: string, patientId: string, patientData: any) {
+  // Error checking
+  const patientDetails = await getPatientDetails(patientId);
+  if (patientDetails === null) throw { status: 400, message: "Invalid patient." };
+  if (patientDetails.researcherId !== researcherId) throw { status: 401, message: "This is not your patient and you are not authorised to update the patient's data." };
+
   const patient = await updatePatientData(patientId, patientData);
   if (patient === null) throw { status: 400, message: "Invalid patient." };
-  if (patient.researcher.id !== researcherId) throw { status: 401, message: "This is not your patient and you are not authorised to update the patient's data." };
 
   return {
     id: patient.id,
