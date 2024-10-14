76971bf6b2ef36432b2dbb48cae1ffc20b9a8066
dzl-i <denzel.iskandar@gmail.com>
2024-10-15 03:46:04 +1100

    feat: simple landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,33 @@
+"use client"
+import Image from "next/image";
+
+import { NavbarGuest } from "@/components/NavbarGuest";
+
+import { TypeAnimation } from "react-type-animation";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>This is the landing page of BiomeData!</h1>
+    <div className="flex flex-col min-h-screen w-full">
+      <NavbarGuest />
+      <main className="flex flex-grow w-full">
+        <div className="w-full grid grid-cols-2">
+          <div className="flex flex-col items-center justify-center ml-32 gap-4">
+            <h1 className="text-5xl font-black text-center">Welcome to BiomeData</h1>
+            <TypeAnimation
+              sequence={["Empowering Biomedical Research Through Unified Data Management"]}
+              wrapper="span"
+              speed={50}
+              repeat={0}
+              className="text-3xl inline-block italic text-center min-h-20"
+            />
+
+            <a href="/register" className="mt-4"><Button color="primary" size="lg">Register Now</Button></a>
+          </div>
+          <div className="flex items-center justify-center">
+            <Image src={"/assets/bioinformatics-cartoon.jpg"} alt="Bioinformatics Cartoon" width={600} height={600} />
+          </div>
+        </div>
       </main>
     </div>
   );
