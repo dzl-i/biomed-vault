6dd7afea19877c55462b6701e0690a5cc60ee986
dzl-i <denzel.iskandar@gmail.com>
2024-10-16 18:38:41 +1100

    feat: initial set up for all pages

--- client/src/app/patients/page.tsx
+++ client/src/app/patients/page.tsx
@@ -3,7 +3,6 @@ import { cookies } from 'next/headers'
 import { Navbar } from '../../components/Navbar';
 import { Button } from '@nextui-org/react';
 import Link from 'next/link';
-
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 export default function Page() {
@@ -19,13 +18,13 @@ export default function Page() {
         <>
           <Navbar researcherId={researcherId} />
           <div className="flex flex-col w-full justify-center items-center pl-20">
-            <p>This is BiomeData&apos;s dashboard</p>
+            <p>This is BiomeData&apos;s patient dataset page</p>
             <p>If you can see this, congratulations! You are a valid user that has logged in. Enjoy!</p>
           </div>
         </>
       ) : (
         <div className="flex flex-col items-center justify-center w-full gap-4">
-          <p>If you see this, you are not logged in yet. Please create an account or log in to view the dashboard :)</p>
+          <p>If you see this, you are not logged in yet. Please create an account or log in to view the patient dataset page :)</p>
           <div className="flex flex-row gap-4">
             <Link href="/register"><Button color="primary">Sign Up</Button></Link>
             <Link href="/login"><Button color="primary">Log In</Button></Link>
--- client/src/app/profile/[id]/page.tsx
+++ client/src/app/profile/[id]/page.tsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import { cookies } from 'next/headers'
-import { Navbar } from '../../components/Navbar';
+import { Navbar } from '../../../components/Navbar';
 import { Button } from '@nextui-org/react';
 import Link from 'next/link';
 
-import jwt, { JwtPayload } from "jsonwebtoken";
-
-export default function Page() {
+export default function Page({ params }: { params: { id: string } }) {
   const cookieStore = cookies();
   const validUser = cookieStore.has("accessToken") || cookieStore.has("refreshToken");
-  const token = cookieStore.get("refreshToken")?.value || "no token";
-  const tokenDecoded = jwt.verify(token, process.env.REFRESH_JWT_SECRET as string) as JwtPayload;
-  const researcherId = tokenDecoded.researcherId as string;
 
   return (
     <div className='flex min-h-screen w-full'>
       {validUser ? (
         <>
-          <Navbar researcherId={researcherId} />
+          <Navbar researcherId={params.id} />
           <div className="flex flex-col w-full justify-center items-center pl-20">
-            <p>This is BiomeData&apos;s dashboard</p>
+            <p>This is BiomeData&apos;s researcher profile page</p>
+            <p>Profile ID is {params.id}</p>
             <p>If you can see this, congratulations! You are a valid user that has logged in. Enjoy!</p>
           </div>
         </>
       ) : (
         <div className="flex flex-col items-center justify-center w-full gap-4">
-          <p>If you see this, you are not logged in yet. Please create an account or log in to view the dashboard :)</p>
+          <p>If you see this, you are not logged in yet. Please create an account or log in to view the researcher profile page :)</p>
           <div className="flex flex-row gap-4">
             <Link href="/register"><Button color="primary">Sign Up</Button></Link>
             <Link href="/login"><Button color="primary">Log In</Button></Link>
