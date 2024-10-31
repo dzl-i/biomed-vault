b253054a3d4085c5c83f9c4da0455061f53b2a7f
dzl-i <denzel.iskandar@gmail.com>
2024-10-31 18:13:05 +1100

    feat: stub page for patient details

---
+++ client/src/app/patient/[id]/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { cookies } from 'next/headers'
+import { Navbar } from '../../../components/Navbar';
+
+import { UnauthenticatedUser } from '@/components/UnauthenticatedUser';
+
+export default function Page({ params }: { params: { id: string } }) {
+  const cookieStore = cookies();
+  const validUser = cookieStore.has("accessToken") || cookieStore.has("refreshToken");
+
+  return (
+    <div className='flex min-h-screen w-full'>
+      {validUser ? (
+        <>
+          <Navbar researcherId={params.id} />
+          <div className="flex flex-col w-full justify-center items-center pl-20">
+            <p>This is BiomeData&apos;s patient overview page</p>
+            <p>Profile ID is {params.id}</p>
+            <p>If you can see this, congratulations! You are a valid user that has logged in. Enjoy!</p>
+          </div>
+        </>
+      ) : (
+        <UnauthenticatedUser />
+      )}
+    </div>
+  )
+}
